3e7cbd04b79352d3cd6d263c0ed8935006614537
geof-matheus <geofmatheus@gmail.com>
2020-10-28 20:52:34 -0300

    add colision between snake and board

--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((width,height))
 
 
 # Board
-def Board(n):
+def board(n):
     for row in range(n):
         for column in range(n):
             pygame.draw.rect(screen, (255, 255, 0),((20*column,20*row), (20,20)), 1)
@@ -22,7 +22,7 @@ food = pygame.Rect((randint(0,39)*20,randint(0,39)*20 ),(20,20))
 runing = True
 while runing:
     screen.fill(black)
-    Board(40)
+    board(40)
     pygame.draw.rect(screen, (255,0,255),snake)
 
     pygame.draw.rect(screen, (255,255,255), food)
@@ -32,6 +32,16 @@ while runing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake.x += 20
+            elif event.key == pygame.K_LEFT:
+                snake.x -= 20
+            elif event.key == pygame.K_UP:
+                snake.y -= 20
+            elif event.key == pygame.K_DOWN:
+                snake.y += 20
 
     key = pygame.key.get_pressed()
 
@@ -43,3 +53,14 @@ while runing:
         snake.x += 20
     if key[pygame.K_a]:
         snake.x -= 20
+
+
+    #colision between snake and board
+    if snake.x > 780:
+        snake.x = 0
+    if snake.x < 0:
+        snake.x = 780
+    if snake.y < 0:
+        snake.y = 780
+    if snake.y > 780:
+        snake.y = 0
